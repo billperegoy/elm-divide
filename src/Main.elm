779b4a24a7dbc1a1c2a4efe8996040ade08d79cf
Bill Peregoy <bill.peregoy@endurance.com>
2017-04-01 14:30:45 -0400

    Logged in user can now select a game.
    When user is logged in, they will have the ability to choose a game from
    the remaining games column. Upon selection, that game will be moved to
    their list of chosen games.

--- src/Main.elm
+++ src/Main.elm
@@ -37,15 +37,24 @@ type alias FlashElement =
 
 
 type alias Ticket =
-    { date : String
+    { id : Int
+    , date : String
     , opponent : String
     , time : String
     , owner : Maybe String
     }
 
 
+type Role
+    = Unregistered
+    | Free
+    | Paid
+    | Admin
+
+
 type alias User =
     { name : String
+    , role : Role
     }
 
 
@@ -59,27 +68,43 @@ type alias Model =
     }
 
 
+nullUser : User
+nullUser =
+    { name = "guest"
+    , role = Unregistered
+    }
+
+
 initTickets : List Ticket
 initTickets =
-    [ Ticket "April 1" "Chicago" "7:10pm" Nothing
-    , Ticket "April 2" "Chicago" "7:10pm" Nothing
-    , Ticket "April 3" "Chicago" "1:05pm" Nothing
-    , Ticket "April 5" "Baltimore" "7:10pm" Nothing
-    , Ticket "April 6" "Baltimore" "7:10pm" Nothing
-    , Ticket "April 7" "Baltimore" "7:10pm" Nothing
-    , Ticket "April 8" "Toronto" "7:10pm" Nothing
-    , Ticket "April 9" "Toronto" "7:10pm" Nothing
-    , Ticket "April 10" "Toronto" "1:05pm" Nothing
+    [ Ticket 0 "April 1" "Chicago" "7:10pm" Nothing
+    , Ticket 1 "April 2" "Chicago" "7:10pm" Nothing
+    , Ticket 2 "April 3" "Chicago" "1:05pm" Nothing
+    , Ticket 3 "April 5" "Baltimore" "7:10pm" (Just "Bill")
+    , Ticket 4 "April 6" "Baltimore" "7:10pm" Nothing
+    , Ticket 5 "April 7" "Baltimore" "7:10pm" (Just "Joe")
+    , Ticket 6 "April 8" "Toronto" "7:10pm" Nothing
+    , Ticket 7 "April 9" "Toronto" "7:10pm" Nothing
+    , Ticket 8 "April 10" "Toronto" "1:05pm" Nothing
     ]
+
+
+myTurn : Model -> Bool
+myTurn model =
+    let
+        currentUserName =
+            userField model model.currentUser .name
+    in
+        currentUserName == model.myName
 
 
 initUsers : Array User
 initUsers =
-    [ User "Joe"
-    , User "Bill"
-    , User "Kelly"
-    , User "Joe"
-    , User "John"
+    [ User "Joe" Paid
+    , User "Bill" Paid
+    , User "Kelly" Paid
+    , User "David" Paid
+    , User "John" Paid
     ]
         |> Array.fromList
 
@@ -103,11 +128,38 @@ init =
 type Msg
     = CreateFlashElement String String Time
     | DeleteFlashElement Int Time
+    | NextUser
+    | SelectTicket Int
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
+        NextUser ->
+            let
+                length =
+                    Array.length model.users
+
+                nextUser =
+                    if model.currentUser == (length - 1) then
+                        0
+                    else
+                        model.currentUser + 1
+
+                nextUserName =
+                    userField model nextUser .name
+
+                color =
+                    if nextUserName == model.myName then
+                        "danger"
+                    else
+                        "info"
+            in
+                { model | currentUser = nextUser }
+                    ! [ Task.succeed (CreateFlashElement (nextUserName ++ "'s turn") color 20)
+                            |> Task.perform identity
+                      ]
+
         CreateFlashElement text color duration ->
             let
                 newFlashElement =
@@ -138,6 +190,21 @@ update msg model =
             in
                 { model | flashElements = newList } ! []
 
+        SelectTicket id ->
+            let
+                newTickets =
+                    List.map (\ticket -> markTicketSelected ticket id model.myName) model.tickets
+            in
+                { model | tickets = newTickets } ! []
+
+
+markTicketSelected : Ticket -> Int -> String -> Ticket
+markTicketSelected ticket id userName =
+    if ticket.id == id then
+        { ticket | owner = Just userName }
+    else
+        ticket
+
 
 deleteCmd : Int -> Time -> Cmd Msg
 deleteCmd id duration =
@@ -158,10 +225,30 @@ durationString val =
         toString val
 
 
-flashView : List FlashElement -> Html Msg
-flashView elements =
-    div []
-        (flashViewElements elements)
+userPlusButton : Model -> List (Html Msg)
+userPlusButton model =
+    [ h3 []
+        [ span [ class "label label-default" ]
+            [ text
+                ("Logged In: " ++ model.myName)
+            ]
+        ]
+    , h3 []
+        [ span [ class "label label-default" ]
+            [ text
+                ("Up Now: " ++ (userField model model.currentUser .name))
+            ]
+        ]
+    , button [ class "btn btn-default", onClick NextUser ] [ text "Next User" ]
+    ]
+
+
+flashView : Model -> Html Msg
+flashView model =
+    div [ class "col-md-2" ]
+        ((userPlusButton model)
+            ++ (flashViewElements model.flashElements)
+        )
 
 
 flashViewElements : List FlashElement -> List (Html Msg)
@@ -177,53 +264,89 @@ flashViewElements elements =
 
 header : Html Msg
 header =
-    div [ class "jumbotron" ] [ h1 [] [ text "Divider" ] ]
+    div [ class "jumbotron text-center" ] [ h1 [] [ text "Dividasaurus" ] ]
 
 
-remainingTickets : List Ticket -> Html Msg
-remainingTickets tickets =
-    div [ class "col-md-6" ]
-        (List.filter
-            (\ticket -> ticket.owner == Nothing)
-            tickets
-            |> List.map
-                (\ticket -> div [] [ text ticket.date ])
-        )
+ticketList : Bool -> List Ticket -> List (Html Msg)
+ticketList myTurn tickets =
+    List.map
+        (\ticket -> singleTicket ticket myTurn)
+        tickets
 
 
-currentUserName : Model -> String
-currentUserName model =
+singleTicket : Ticket -> Bool -> Html Msg
+singleTicket ticket myTurn =
     let
-        lookup =
-            Array.get model.currentUser model.users
+        innerDiv =
+            div [ class "panel panel-default" ]
+                [ div [ class "panel-body" ]
+                    [ text ticket.date ]
+                ]
     in
-        case lookup of
-            Nothing ->
-                "unknown"
+        if myTurn then
+            a [ onClick (SelectTicket ticket.id), href "#" ]
+                [ innerDiv
+                ]
+        else
+            innerDiv
 
-            Just user ->
-                user.name
+
+remainingTickets : List Ticket -> Bool -> Html Msg
+remainingTickets tickets myTurn =
+    div
+        [ class "col-md-4" ]
+        [ h1 [ style [ ( "padding-bottom", "10px" ) ] ]
+            [ span
+                [ class "label label-primary" ]
+                [ text "Remaining Tickets" ]
+            ]
+        , div []
+            (List.filter
+                (\ticket -> ticket.owner == Nothing)
+                tickets
+                |> ticketList myTurn
+            )
+        ]
+
+
+userField : Model -> Int -> (User -> a) -> a
+userField model index extractor =
+    Array.get index model.users
+        |> Maybe.withDefault nullUser
+        |> extractor
 
 
 myTickets : Model -> Html Msg
 myTickets model =
-    div [ class "col-md-6" ]
-        [ div [] [ text ("It's " ++ currentUserName model ++ "'s turn") ]
-        , div [] [ text model.myName ]
+    div [ class "col-md-4" ]
+        [ h1 [ style [ ( "padding-bottom", "10px" ) ] ]
+            [ span
+                [ class "label label-primary" ]
+                [ text "My Tickets" ]
+            ]
+        , div []
+            (List.filter
+                (\ticket -> ticket.owner == Just model.myName)
+                model.tickets
+                |> ticketList False
+            )
         ]
 
 
 view : Model -> Html Msg
 view model =
-    div [ class "container" ]
-        [ header
-        , div [ class "row" ]
-            [ flashView model.flashElements ]
-        , div [ class "row" ]
-            [ remainingTickets model.tickets
-            , myTickets model
+    let
+        itsMyTurn =
+            myTurn model
+    in
+        div [ class "container" ]
+            [ header
+            , div [ class "row" ]
+                [ flashView model
+                , remainingTickets model.tickets itsMyTurn
+                , myTickets model
+                ]
             ]
-        ]
 
 
 
