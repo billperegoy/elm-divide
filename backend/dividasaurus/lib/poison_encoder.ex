7d90a55d911921a250c1b9afe38687d6ff8cbfce
Bill Peregoy <bill.peregoy@endurance.com>
2017-04-03 20:33:23 -0400

    Minor refactor of poison encoder.
    Removed the intermediate variable and moved to the infinately more
    elegant pipe.

--- backend/dividasaurus/lib/poison_encoder.ex
+++ backend/dividasaurus/lib/poison_encoder.ex
@@ -1,10 +1,10 @@
 defimpl Poison.Encoder, for: Any do
   def encode(%{__struct__: _} = struct, options) do
-    map = struct
-          |> Map.from_struct
-          |> sanitize_map
-          |> remove_associations
-    Poison.Encoder.Map.encode(map, options)
+    struct
+      |> Map.from_struct
+      |> sanitize_map
+      |> remove_associations
+      |> Poison.Encoder.Map.encode(options)
   end
 
   defp sanitize_map(map) do
