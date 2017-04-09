acbc4ea54888c0dd0a141fdaa0c29df39864ed80
Bill Peregoy <bill.peregoy@endurance.com>
2017-04-09 07:04:50 -0400

    Fixed syntax error in config/prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -16,7 +16,7 @@ config :dividasaurus, Dividasaurus.Endpoint,
   url: [host: "example.com", port: 80],
   url: [scheme: "https", host: "dry-gorge-98580.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
-  cache_static_manifest: "priv/static/manifest.json"
+  cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :dividasaurus, Dividasaurus.Repo,
