f0834b2c93af9dbdf3c3318838da7c245f8c8adf
Bill Peregoy <bill@peregoy.org>
2017-04-12 19:51:00 -0400

    Added a static page at root
    This page is the placeholder for the Elm app. This still needs to be
    integrated with the asset pipeline to have the Elm compiler build the
    bundle in the correct location.

--- web/router.ex
+++ web/router.ex
@@ -1,8 +1,22 @@
 defmodule Dividasaurus.Router do
   use Dividasaurus.Web, :router
 
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
+  end
+
+  scope "/", Dividasaurus do
+    pipe_through :browser
+
+    get "/", PageController, :index
   end
 
   scope "/api/v1", Dividasaurus do
