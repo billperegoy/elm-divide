8c426b03c858705c70a189c6c80ce6b5e0fffd06
Bill Peregoy <bill@peregoy.org>
2017-04-12 21:40:45 -0400

    Cleaned up brunch config file

--- brunch-config.js
+++ brunch-config.js
@@ -57,26 +57,11 @@ exports.config = {
       ignore: [/web\/static\/vendor/]
     },
     elmBrunch: {
-      // (required) Set to the elm file(s) containing your "main" function `elm make` 
-      //            handles all elm dependencies relative to `elmFolder`
-      mainModules: ['src/Main.elm'],
-
-      // (optional) Set to path where `elm-make` is located, relative to `elmFolder`
       executablePath: '../node_modules/elm/binwrappers',
-
-      // (optional) Set to path where elm-package.json is located, defaults to project root
-      //            if your elm files are not in /app then make sure to configure 
-      //            paths.watched in main brunch config
       elmFolder: 'frontend',
-
-      // (optional) Defaults to 'js/' folder in paths.public
+      mainModules: ['src/Main.elm'],
       outputFolder: '../priv/static/js',
-
-      // (optional) If specified, all mainModules will be compiled to a single file 
-      //            This is merged with outputFolder.
       outputFile: 'bundle.js',
-
-      // (optional) add some parameters that are passed to elm-make
       makeParameters: ['--warn']
     }
   },
